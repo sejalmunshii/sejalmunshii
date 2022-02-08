93838b640fe562ad827f72ceaca34d519aee7170
sejalmunshii <98741677+sejalmunshii@users.noreply.github.com>
2022-02-08 19:18:10 +0530

    Create home_page.php

---
+++ home_page.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Phoenix Market City </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="h_style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+</head>
+
+<body>
+  
+	
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+   <nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">
+    <img src=" logo.png" width="100" height="100" alt="">
+  </a>
+</nav >
+
+ 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">  <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"> Bill-pay </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"> Why us? </a>
+      </li>
+      <li class="nav-item dropdown">
+         <li class="nav-item">
+        <a class="nav-link" href="#"> Credit? </a>
+      </li>
+       <li class="nav-item dropdown">
+         <li class="nav-item">
+        <a class="nav-link" href="#"> Help </a>
+      </li>
+    </ul>
+     <form class="form-inline my-2 my-lg-0">
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto" >
+      <li class="nav-item dropdown">
+         <li class="nav-item">
+        <a  class="nav-link" href="Phoenix_loginpage.php">LOG IN </a>
+      </li>
+      </ul>
+     <form class="form-inline my-2 my-lg-0">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign up</button>
+    </form>
+  </div>
+</nav>
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="c_bg.jpg" alt="" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1 class="font-weight-bold">Let's design that future together</h1>
+
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="c_bg2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1  class="font-weight-bold">Hungry for something creative</h1>
+        
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="c_bg4.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1 class="font-weight-bold">Values are more expensive than price</h1>
+       
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script></title>
+</body>
+</html>
